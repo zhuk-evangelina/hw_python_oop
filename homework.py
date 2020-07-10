7fc042fb4e04b4b7e26aa9b3589a2128e9af634a
zhuk-evangelina <zhuk.evangelina@yandex.ru>
2020-07-10 23:38:44 +0200

    review fixes 2

--- homework.py
+++ homework.py
@@ -9,6 +9,7 @@ class Record:
         else:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
 
+
 class Calculator:
     def __init__(self, limit):
         self.limit = limit
@@ -19,43 +20,44 @@ class Calculator:
 
     def get_today_stats(self):
         now = dt.date.today()
-        today_stats = [record.amount for record in self.records if record.date == now]
+        today_stats = [record.amount for record in self.records 
+            if record.date == now]
         return sum(today_stats)  
              
     def get_week_stats(self):
         now = dt.date.today()
         week_ago = now - dt.timedelta(days = 7)
-        week_stats = 0
-        for record in self.records:
-            if week_ago <= record.date <= now:
-                week_stats += record.amount
-        return week_stats
+        week_stats = [record.amount for record in self.records 
+            if week_ago <= record.date <= now]
+        return sum(week_stats)
     
     def get_today_remained(self):
         return self.limit - self.get_today_stats()
+
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         remainder = self.get_today_remained()
         if remainder > 0:
-            return (f'Сегодня можно съесть что-нибудь ещё, '
+            return ('Сегодня можно съесть что-нибудь ещё, '
                 f'но с общей калорийностью не более {remainder} кКал')
         return 'Хватит есть!'
    
+
 class CashCalculator(Calculator):
     USD_RATE = 60.0
     EURO_RATE = 70.0
     RUB_RATE = 1.0
 
     currencies = {
-    'eur': ('Euro', EURO_RATE),
-    'usd': ('USD', USD_RATE),
-    'rub': ('руб', RUB_RATE),
-}
+        'eur': ('Euro', EURO_RATE),
+        'usd': ('USD', USD_RATE),
+        'rub': ('руб', RUB_RATE),
+    }
 
     def get_today_cash_remained(self, currency):
         currency_text, currency_rate = self.currencies[currency]    
-        remainder = round (self.get_today_remained() / currency_rate, 2)
+        remainder = round(self.get_today_remained() / currency_rate, 2)
 
         if not remainder: 
             return 'Денег нет, держись'
